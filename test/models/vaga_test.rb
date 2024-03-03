434f26d55f9f15b867d38fe42100971c006c93c4
Gabriel <josegabrielpereira2017@gmail.com>
2024-03-03 15:18:44 -0300

    Testes de model de VAGA

--- test/models/vaga_test.rb
+++ test/models/vaga_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class VagaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "salvar vaga com dados validos" do
+    empregador = Empregador.new(nome: 'João Grilo', nomeEmpresa: 'Padaria Chico e João Grilo', email: 'deixe@disso.homi', cnpj: '12.345.678/0001-90', endereco:'Algum lugar do sertão nordestino', telefone: '87912345678')
+    assert empregador.save
+
+    vaga = empregador.vagas.build(titulo: 'Cangaceiro', descricao: 'Só caba valente e com sangue nos olhos.', salario: 1000.00)
+    assert vaga.save
+  end
+
+  test "salvar vaga com valor de salario invalido" do
+    empregador = Empregador.new(nome: 'João Grilo', nomeEmpresa: 'Padaria Chico e João Grilo', email: 'deixe@disso.homi', cnpj: '12.345.678/0001-90', endereco:'Algum lugar do sertão nordestino', telefone: '87912345678')
+    assert empregador.save
+
+    vaga = empregador.vagas.build(titulo: 'Cangaceiro', descricao: 'Só caba valente e com sangue nos olhos.', salario: -1)
+    assert_not vaga.save
+  end
 end
