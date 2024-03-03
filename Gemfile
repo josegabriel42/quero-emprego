a86d3bb8ecb351e2e53a1d3aa990951ee98c7b9d
Gabriel <josegabrielpereira2017@gmail.com>
2024-03-03 10:43:32 -0300

    Testes de model de EMPREGADOR

--- Gemfile
+++ Gemfile
@@ -68,7 +68,7 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
-  gem "cucumber-rails"
+  gem "cucumber-rails", require: false
   gem "database_cleaner"
   gem "rspec"
 end
--- test/models/empregador_test.rb
+++ test/models/empregador_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class EmpregadorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "salvar empregador com dados validos" do
+    empregador = Empregador.new(nome: 'João Grilo', nomeEmpresa: 'Padaria Chico e João Grilo', email: 'deixe@disso.homi', cnpj: '12.345.678/0001-90', endereco:'Algum lugar do sertão nordestino', telefone: '87912345678')
+    assert empregador.save
+  end
+
+  test "salvar candidato com cnpj ja em uso" do
+    empregador1 = Empregador.new(nome: 'Chico', nomeEmpresa: 'Padaria Chico e João Grilo', email: 'num@sei.oxi', cnpj: '11.111.111/0001-11', endereco:'Algum lugar do sertão nordestino', telefone: '87912345678')
+    assert empregador1.save
+    empregador2 = Empregador.new(nome: 'Chico', nomeEmpresa: 'Padaria Chico e João Grilo', email: 'num@sei.oxi', cnpj: '11.111.111/0001-11', endereco:'Algum lugar do sertão nordestino', telefone: '87912345678')
+    assert_not empregador2.save
+  end
 end
